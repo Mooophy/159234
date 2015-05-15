95fca0634548d34d0b0930d25c2cb6b3e7d7bc4d
Yue Wang <Moophyalan@gmail.com>
2015-05-15 18:32:07 +1200

    Update A2P2.h

--- a2p2/a2p2/A2P2.h
+++ a2p2/a2p2/A2P2.h
@@ -1,4 +1,5 @@
-#pragma once 
+#pragma once
+#include <iomanip>
 #include <iostream>
 
 
@@ -6,86 +7,114 @@ template<typename ET>
 class VecT
 {
 public:
-    //
-    // constructors
-    //
-    VecT(int sz = 5)
-        : size{ sz }, ptr{ new ET[sz]() }
-    {}
-    VecT(VecT const& other)
-        : size{ sz }, ptr{ new ET[sz]() }
-    {
-        for (int i = 0; i != size; ++i) ptr[i] = other[i];
-    }
-    VecT(VecT && other) noexcept
-        : size{ sz }, ptr{ new ET[sz]() }
-    {
-        for (int i = 0; i != size; ++i) ptr[i] = std::move(other[i]);
-        other.ptr = nullptr;
-    }
+	//
+	// constructors
+	//
+	VecT(int sz = 5)
+		: size{ sz }, ptr{ new ET[sz]() }
+	{}
+	VecT(VecT const& other)
+		: size{ other.getSize() }, ptr{ new ET[other.getSize()]() }
+	{
+		for (int i = 0; i != size; ++i) ptr[i] = other[i];
+	}
+	VecT(VecT && other) noexcept
+		: size{ other.getSize() }, ptr{ new ET[other.getSize()]() }
+	{
+		for (int i = 0; i != size; ++i) ptr[i] = std::move(other[i]);
+		other.ptr = nullptr;
+	}
 
-        //
-        // destructor
-        //
-        ~VecT()
-    {
-        delete[] ptr;
-    }
+	//
+	// destructor
+	//
+	~VecT()
+	{
+		delete[] ptr;
+	}
 
-    //
-    // operator overloading
-    //
-    VecT& operator=(VecT const& other)
-    {
-        if (this->ptr != other.ptr)
-        {
-            delete[] this->ptr;
-            size = other.size();
-            ptr = new ET[sz]();
-            for (int i = 0; i != size; ++i) ptr[i] = std::move(other[i]);
-        }
-        other.ptr = nullptr;
-        return *this;
-    }
-    VecT& operator=(VecT && other)
-    {
-        if (this->ptr != other.ptr)
-        {
-            delete[] this->ptr;
-            size = other.size();
-            ptr = new ET[sz]();
-            for (int i = 0; i != size; ++i) ptr[i] = other[i];
-        }
-    }
-    bool operator==(VecT const& other) const
-    {
-        if (this->size != other.size) return false;
-        for (int i = 0; i != size; ++i) if (ptr[i] != other[i]) return false;
-        return true;
-    }
-    bool operator!=(VecT const& other) const
-    {
-        return !(*this == other);
-    }
-    ET& operator[](int) 
-    { 
-        return ptr[i]; 
-    }
-    ET const& operator[](int i) const 
-    { 
-        return ptr[i] 
-    };
-    //
-    // public interfaces
-    //
-    int getSize() const
-    {
-        return size;
-    }
+	//
+	// operator overloading
+	//
+	VecT& operator=(VecT const& other)
+	{
+		if (this->ptr != other.ptr)
+		{
+			delete[] this->ptr;
+			size = other.size();
+			ptr = new ET[size]();
+			for (int i = 0; i != size; ++i) ptr[i] = std::move(other[i]);
+		}
+		other.ptr = nullptr;
+		return *this;
+	}
+	VecT& operator=(VecT && other)
+	{
+		if (this->ptr != other.ptr)
+		{
+			delete[] this->ptr;
+			size = other.size();
+			ptr = new ET[size]();
+			for (int i = 0; i != size; ++i) ptr[i] = other[i];
+		}
+	}
+	bool operator==(VecT const& other) const
+	{
+		if (this->size != other.size) return false;
+		for (int i = 0; i != size; ++i) if (ptr[i] != other[i]) return false;
+		return true;
+	}
+	bool operator!=(VecT const& other) const
+	{
+		return !(*this == other);
+	}
+	ET& operator[](int i) 
+	{ 
+		return ptr[i]; 
+	}
+	ET const& operator[](int i) const 
+	{ 
+		return ptr[i]; 
+	};
+	//
+	// public interfaces
+	//
+	int getSize() const
+	{
+		return size;
+	}
 private:
-    //
-    // data members
-    //
-    int size;
-    ET* ptr;
+	//
+	// data members
+	//
+	int size;
+	ET* ptr;
 };
+
+//
+// non member functions 
+//
+template<typename T>
+inline std::istream& operator>>(std::istream& is, VecT<T> const& vec)
+{
+	for(int i = 0; i != vec.getSize(); ++i) is >> vec[i];
+	return is;
+}
+
+template<typename T>
+inline std::ostream& operator<<(std::ostream& os, VecT<T> const& vec)
+{
+	int width = 10;
+	os << std::setw(width);
+	for(int i = 0; i != vec.getSize(); ++i) os << vec[i];
+	return os;
+}
+
+void info()
+{
+	std::cout << "*******************************************" << std::endl;
+	std::cout << "*     159.234 Assignment 2 - Part 2       *" << std::endl;
+	std::cout << "*     12086075 , Kuan Liu                 *" << std::endl;
+	std::cout << "*     13158665 , Yuxiang Zhang            *" << std::endl;
+	std::cout << "*******************************************" << std::endl;
+}
