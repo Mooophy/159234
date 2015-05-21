a584343788d6fb1e743fb8269c6edb75b53d069e
moophy <MoophyAlan@gmail.com>
2015-05-22 11:10:26 +1200

    use intellij

--- as3/src/movie/Main.java
+++ as3/src/movie/Main.java
@@ -1,14 +1,15 @@
 package movie;
 
-public class Main {
+public class Main
+{
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) 
-	{
-		movie.Movie m = new movie.Movie();
-		System.out.println("testing\n"); 
-	}
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        movie.Movie m = new movie.Movie();
+        System.out.println("testingzz\n");
+    }
 
 }
--- as3/src/movie/Movie.java
+++ as3/src/movie/Movie.java
@@ -1,19 +1,18 @@
 package movie;
 
-public class Movie 
-{	
-	public void setTitle(String t){ this.title = t; }
-	public void setDirctor(String d){ this.director = d; }
-	public void setQuality(int q){ this.quality = q; }
-	@Override
-	public String toString()
-	{
-		return "\nTitle: " + this.title + "\nDirector: " + this.director + "\nTime: " + this.time + " mins\nQuality: " + "*****".substring(0, this.quality);
-	}
-	public void display(){ System.out.println(this.toString()); }
-	
-	
-	private String title, director;
-	private int time, quality;
+public class Movie
+{
+    public void setTitle(String t){ this.title = t; }
+    public void setDirctor(String d){ this.director = d; }
+    public void setQuality(int q){ this.quality = q; }
+    @Override
+    public String toString()
+    {
+        return "\nTitle: " + this.title + "\nDirector: " + this.director + "\nTime: " + this.time + " mins\nQuality: " + "*****".substring(0, this.quality);
+    }
+    public void display(){ System.out.println(this.toString()); }
 
+
+    private String title, director;
+    private int time, quality;
 }
