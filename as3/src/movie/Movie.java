abb85d4fdaa1acac1858de50271b2cc53fced022
moophy <MoophyAlan@gmail.com>
2015-05-22 11:41:59 +1200

    modified:   as3/src/movie/Movie.java

--- as3/src/movie/Movie.java
+++ as3/src/movie/Movie.java
@@ -7,12 +7,15 @@ public class Movie
     public void setTitle(String t){ this.title = t; }
     public void setDirctor(String d){ this.director = d; }
     public void setQuality(int q){ this.quality = q; }
+
     @Override
     public String toString()
     {
         return "\nTitle: " + this.title + "\nDirector: " + this.director + "\nTime: " + this.time + " mins\nQuality: " + "*****".substring(0, this.quality);
     }
+    
     public void display(){ System.out.println(this.toString()); }
+
     public  void getData(String filename)
     {
         BufferedReader buffer = null;
