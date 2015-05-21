ba9ecbb3b8fd7582d9208bf5e55991c1ba962578
moophy <MoophyAlan@gmail.com>
2015-05-22 11:38:21 +1200

    modified:   as3/src/movie/Movie.java

--- as3/src/movie/Movie.java
+++ as3/src/movie/Movie.java
@@ -1,5 +1,7 @@
 package movie;
 
+import java.io.*;
+
 public class Movie
 {
     public void setTitle(String t){ this.title = t; }
@@ -11,7 +13,31 @@ public class Movie
         return "\nTitle: " + this.title + "\nDirector: " + this.director + "\nTime: " + this.time + " mins\nQuality: " + "*****".substring(0, this.quality);
     }
     public void display(){ System.out.println(this.toString()); }
+    public  void getData(String filename)
+    {
+        BufferedReader buffer = null;
 
+        try
+        {
+            buffer = new BufferedReader(new FileReader(filename));
+        }
+        catch (FileNotFoundException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        try
+        {
+            this.title = buffer.readLine();
+            this.director = buffer.readLine();
+            this.time = Integer.parseInt(buffer.readLine());
+            this.quality = Integer.parseInt(buffer.readLine());
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
 
     private String title, director;
     private int time, quality;
